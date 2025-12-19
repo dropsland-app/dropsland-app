d87093db2d87639d402fe01c4fedf8f7856195b4
Steven Molina <stevenmolina2205@gmail.com>
2025-12-19 03:16:00 -0300

    feat(PWA): add icons config

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="application-name" content="Your App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Your App" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </head>
       <body className={inter.className}>
         <PrivyProviderWrapper>
           <ThemeProvider
